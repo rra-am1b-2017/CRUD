3a75583a8514ba49b80e7004280e5bcf6f2c0de9
Arjan de Ruijter <rra.am1a.2017@gmail.com>
2017-11-22 15:35:16 +0100

    main.container

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
   </head>
-  <body>   
+  <body> 
+    <main class="container">  
     <?php
       
       // $voornaam = "Arjan";
@@ -145,6 +146,8 @@
         <?php echo $table_content; ?>
       </tbody>    
     </table>
+
+    </main>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
