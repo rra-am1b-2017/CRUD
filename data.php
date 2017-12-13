0050b29cdbf048e1e2e48ae9c0a1a97c86e56215
Esteban Vihaio <a.de.ruijter@mboutrecht.nl>
2017-12-13 14:53:10 +0100

    Week6 sanitize toegevoegd halverwege les 13-12-2017 wo

--- data.php
+++ data.php
@@ -1,24 +1,16 @@
 <?php
   // Maak contact met de mysql-server
   include("./connect_db.php");
+  include("./functions/function.php");
 
-
+  // We maken de $_POST-variabelen schoon door ze door de sanitize function te halen
   $voornaam       = sanitize($_POST['voornaam']);
   $tussenvoegsel  = sanitize($_POST['tussenvoegsel']);
   $achternaam     = sanitize($_POST['achternaam']);
   $leeftijd       = sanitize($_POST['leeftijd']);
-  $schoenmaat     = sanitize($_POST['schoenmaat']);
-
-
-  function sanitize($data) {
-    // addslashes maakt enkele quotjes (') en dubbele (") en backslashes (\) onschadelijk
-    $data = addslashes($data);
-    
-    // htmlspecialchars maakt html tags onschadelijk
-    $data = htmlspecialchars($data);
-    return $data;
-  }
+  $schoenmaat     = sanitize($_POST['schoenmaat']); 
   
+  // De schoongemaakte $_POST-variabelen worden gebruikt in de sql-query
   $sql = "INSERT INTO `users`  (`id`,
                                 `voornaam`, 
                                 `tussenvoegsel`, 
@@ -32,7 +24,7 @@
                                 '$leeftijd',
                                 '$schoenmaat')";
 
-  //echo $sql; exit();
+  // echo $sql; exit();
   echo "De gegevens zijn goed weggeschreven naar de database";
 
   mysqli_query($conn, $sql);
